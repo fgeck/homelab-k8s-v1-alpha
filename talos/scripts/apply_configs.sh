9e6189158a7530fec6766d304dd866578a6f7e37
floge77 <floge77@gmail.com>
2024-12-21 19:34:55 +0100

    Fix generate_config and apply_config

--- talos/scripts/apply_configs.sh
+++ talos/scripts/apply_configs.sh
@@ -7,32 +7,39 @@ set -e
 # when the OS was installed. Can be found in the VM screen
 # Run the script from the root of this repository. It will fetch the final IPs from the config files
 
-CURRENT_CONTROL_PLANE_IP=192.168.178.192
-CURRENT_WORKER_IP=192.168.178.131
+CURRENT_CONTROL_PLANE_IP=
+CURRENT_WORKER_IP=
 
-FINAL_CONTROL_PLANE_IP=$(yq '.machine.network.interfaces[0].addresses[0]' talos/_out/controlplane.yaml | cut -d'/' -f1)
-FINAL_WORKER_IP=$(yq '.machine.network.interfaces[0].addresses[0]' talos/_out/worker.yaml | cut -d'/' -f1)
+FINAL_CONTROL_PLANE_IP=$(yq eval '.machine.network.interfaces[0].addresses[0]' talos/_out/controlplane.yaml | cut -d'/' -f1)
+FINAL_WORKER_IP=$(yq eval '.machine.network.interfaces[0].addresses[0]' talos/_out/worker.yaml | cut -d'/' -f1)
 
-# Caution this needs to be verified!
-if curl -s --connect-timeout 3 "https://$FINAL_CONTROL_PLANE_IP:50000" >/dev/null 2>&1; then
-  echo "Final Control Plane IP is reachable: $FINAL_CONTROL_PLANE_IP"
-  CONTROL_PLANE_IP=$FINAL_CONTROL_PLANE_IP
-else
-  echo "Final Control Plane IP is not reachable, falling back to current IP: $CURRENT_CONTROL_PLANE_IP"
-  CONTROL_PLANE_IP=$CURRENT_CONTROL_PLANE_IP
-fi
+check_ip_reachability() {
+  local ip="$1"
+  local fallback_ip="$2"
 
-if curl -s --connect-timeout 3 "https://$FINAL_WORKER_IP:50000" >/dev/null 2>&1; then
-  echo "Final Worker IP is reachable: $FINAL_WORKER_IP"
-  WORKER_IP=$FINAL_WORKER_IP
-else
-  echo "Final Worker IP is not reachable, falling back to current IP: $CURRENT_WORKER_IP"
-  WORKER_IP=$CURRENT_WORKER_IP
-fi
+  
+  # Run curl and capture the response
+  response=$(curl -s -o /dev/null --connect-timeout 3 -w "%{http_code}" "http://$ip:50000")
+  
+  # Check if the response indicates an empty reply
+  if [[ $response == "000" ]]; then
+    #echo "Final $role IP is reachable: $ip"
+    echo "$ip"
+  else
+    #echo "Final $role IP is not reachable, falling back to current IP: $fallback_ip"
+    echo "$fallback_ip"
+  fi
+}
+
+# Determine the Control Plane IP
+CONTROL_PLANE_IP=$(check_ip_reachability "$FINAL_CONTROL_PLANE_IP" "$CURRENT_CONTROL_PLANE_IP")
+# Determine the Worker IP
+WORKER_IP=$(check_ip_reachability "$FINAL_WORKER_IP" "$CURRENT_WORKER_IP")
+
 
 echo "Applying the final configs to the nodes..."
-echo "  Control Plane: $CURRENT_CONTROL_PLANE_IP"
-echo "  Worker: $CURRENT_WORKER_IP"
+echo "  Control Plane: $CONTROL_PLANE_IP"
+echo "  Worker: $WORKER_IP"
 
 talosctl apply-config --insecure --nodes "$CONTROL_PLANE_IP" --file talos/_out/controlplane.yaml
 talosctl apply-config --insecure --nodes "$WORKER_IP" --file talos/_out/worker.yaml
@@ -42,6 +49,8 @@ sleep 120
 
 talosctl config endpoint "$FINAL_CONTROL_PLANE_IP"
 talosctl config node "$FINAL_CONTROL_PLANE_IP"
+
+echo "Bootstrapping the cluster now"
 
 talosctl bootstrap
 
--- talos/scripts/generate_configs.sh
+++ talos/scripts/generate_configs.sh
@@ -5,15 +5,18 @@
 # 2. set your static IP addresses and the gateway in the variables below
 # 3. run this script from the root of this repository
 
+CLUSTER_NAME=""
 CONTROL_PLANE_IP=""
 WORKER_IP=""
 GATEWAY=""
+# v1.9.0 Contains qemu-guest-agent, iscsi-tools 
+IMAGE="factory.talos.dev/installer/dc7b152cb3ea99b821fcb7340ce7168313ce393d663740b791c36f6e95fc8586:v1.9.0"
 
 mkdir -p talos/_out
 
-talosctl gen config fgeck-k8s https://${CONTROL_PLANE_IP}:6443 \
-    --config-patch-control-plane "[{\"op\": \"add\", \"path\": \"/machine/network/interfaces\", \"value\": [{\"interface\": \"eth0\", \"addresses\": [\"${CONTROL_PLANE_IP}/24\"], \"routes\": [{\"network\": \"0.0.0.0/0\", \"gateway\": \"${GATEWAY}\"}]}]}]" \
-    --config-patch-worker "[{\"op\": \"add\", \"path\": \"/machine/network/interfaces\", \"value\": [{\"interface\": \"eth0\", \"addresses\": [\"${WORKER_IP}/24\"], \"routes\": [{\"network\": \"0.0.0.0/0\", \"gateway\": \"${GATEWAY}\"}]}]}]" \
+talosctl gen config $CLUSTER_NAME https://${CONTROL_PLANE_IP}:6443 \
+    --config-patch-control-plane "[{\"op\": \"add\", \"path\": \"/machine/network/interfaces\", \"value\": [{\"interface\": \"eth0\", \"addresses\": [\"${CONTROL_PLANE_IP}/24\"], \"routes\": [{\"network\": \"0.0.0.0/0\", \"gateway\": \"${GATEWAY}\"}]}]}, {\"op\": \"add\", \"path\": \"/machine/install/image\", \"value\": \"${IMAGE}\"}]" \
+    --config-patch-worker "[{\"op\": \"add\", \"path\": \"/machine/network/interfaces\", \"value\": [{\"interface\": \"eth0\", \"addresses\": [\"${WORKER_IP}/24\"], \"routes\": [{\"network\": \"0.0.0.0/0\", \"gateway\": \"${GATEWAY}\"}]}]}, {\"op\": \"add\", \"path\": \"/machine/install/image\", \"value\": \"${IMAGE}\"}]" \
     --with-secrets ./secrets/secrets.yaml \
     -o talos/_out/
 
