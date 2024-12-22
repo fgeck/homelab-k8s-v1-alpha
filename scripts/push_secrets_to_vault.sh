ffa06a50f1a6db6458fa78e2cdada553b4d2a420
Florian Geckeler <f.geckeler@sap.com>
2024-12-22 22:52:34 +0100

    Creation of secret working

--- scripts/push_secrets_to_vault.sh
+++ scripts/push_secrets_to_vault.sh
@@ -19,9 +19,9 @@ fi
 source ./scripts/helper_funcs.sh
 
 log_debug "Setting your server in config and logging in"
-bw config server "$VAULTWARDEN_SERVER"
-SESSION=$(bw login --raw)
-trap bw_logout EXIT
+# bw config server "$VAULTWARDEN_SERVER"
+SESSION=7j0mLA+i2RieIdId9gjQPqF/WqAnVV3tFmSg6/oP7el+/Gr0ZegI382xOPvGOQbypkk+mZHUWmoZi+3w1jZxmQ== #$(bw login --raw)
+# trap bw_logout EXIT
 
 SECRETS_BASE64=$(cat secrets/secrets.yaml | base64)
 VALUES_BASE64=$(cat secrets/values.yaml | base64)
@@ -31,7 +31,7 @@ echo $ITEM_ID
 if [[ "$ITEM_ID" == "null" ]]; then
     log_debug "Item with name '$SECRET_NAME' not found."
     log_info "Creating new item with name '$SECRET_NAME'"
-    # Todo create item with BW
+    bw get template item --session $SESSION | jq ".name=\"$SECRET_NAME\" | .type = 2 | .secureNote.type = 0 | .notes=\"Base64 encoded Kubernetes values and TalOS secrets\" | .fields += [{\"name\": \"values.yaml\", \"value\": \"$VALUES_BASE64\", \"type\": 1}, {\"name\": \"secrets.yaml\", \"value\": \"$SECRETS_BASE64\", \"type\": 1}]" | bw encode | bw create item --session $SESSION
 else
     log_debug "Item with name '$SECRET_NAME' found."
     log_info "Updating existing item with name '$SECRET_NAME'"
