cad8bdc59a594020ef25c7c3a0abec4bce549ae6
Florian Geckeler <f.geckeler@sap.com>
2024-12-26 23:15:04 +0100

    Skelleton for postgres

---
+++ scripts/create_nfs_dirs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p \
+    /share/traefik \
+    /share/cert-manager \
+    /share/uptime-kuma \
+    /share/signoz \
+    /share/monitoring-postgres \
+    /share/media-postgres \
+    /share/vaultwarden-postgres
