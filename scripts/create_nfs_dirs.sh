248e32ba1b9a5f10b350d037f1e5f9f1b227a4e3
Florian Geckeler <f.geckeler@sap.com>
2024-12-29 00:18:32 +0100

    Sabnzbd working but logging "[Errno 8] Bad file descriptor"

--- scripts/create_nfs_dirs.sh
+++ scripts/create_nfs_dirs.sh
@@ -3,4 +3,9 @@
 set -e
 
 mkdir -p \
-    /share/data
+    /share/data \
+    /share/data/media \
+    /share/data/photos \
+    /share/data/usenet/Downloads/incomplete \
+    /share/data/usenet/Downloads/complete
+
--- scripts/setup_cluster.sh
+++ scripts/setup_cluster.sh
@@ -31,18 +31,16 @@ chart_dir="$script_dir/helm"
 build_helm_dependencies "$chart_dir"
 
 log_success "-------------------------DONE-HELM-REPO-ADD/UPDATE-DEPDENCY-BUILD-------------------------"
-
+echo ""
 log_info "-------------------------START-HELM-UPGRADE-------------------------"
 
 helm upgrade bootstrap "$script_dir/helm/1-bootstrap" --install --namespace kube-system -f $script_dir/helm/1-bootstrap/values.yaml -f $script_dir/secrets/values.yaml
-
 helm upgrade edge "$script_dir/helm/2-edge" --install  --namespace edge --create-namespace -f $script_dir/helm/2-edge/values.yaml -f $script_dir/secrets/values.yaml
 helm upgrade edge-custom "$script_dir/helm/3-edge-custom" --install --namespace edge -f $script_dir/secrets/values.yaml
-
 helm upgrade storage "$script_dir/helm/4-storage" --install --namespace longhorn-system --create-namespace -f $script_dir/helm/4-storage/values.yaml -f $script_dir/secrets/values.yaml
 helm upgrade monitoring "$script_dir/helm/5-monitoring" --install --namespace monitoring --create-namespace -f $script_dir/helm/5-monitoring/values.yaml -f $script_dir/secrets/values.yaml
+helm upgrade media "$script_dir/helm/6-media" --install --namespace media --create-namespace -f $script_dir/helm/6-media/values.yaml -f $script_dir/secrets/values.yaml
 
-# helm upgrade media "$script_dir/helm/6-media" --install --create-namespace -f $script_dir/helm/6-media/values.yaml -f $script_dir/secrets/values.yaml --namespace media
 # helm upgrade apps "$script_dir/helm/7-apps" --install -f $script_dir/helm/7-apps/values.yaml -f $script_dir/secrets/values.yaml
 # kubectl apply -f "$script_dir/helm/apps/nginx.yaml"
 # kubectl apply -f "$script_dir/helm/apps/whoami-local.yaml"
