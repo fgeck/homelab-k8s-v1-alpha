dc1e924c64302ecd3d320be91adca9b58d9218aa
Florian Geckeler <f.geckeler@sap.com>
2024-12-28 22:05:23 +0100

    Uptime-kuma with longhorn volume working

--- scripts/create_nfs_dirs.sh
+++ scripts/create_nfs_dirs.sh
@@ -3,15 +3,4 @@
 set -e
 
 mkdir -p \
-    /share/data \
-    /share/edge/traefik \
-    /share/edge/cert-manager \
-    /share/monitoring/uptime-kuma \
-    /share/monitoring/signoz \
-    /share/monitoring-postgres \
-    /share/media/postgres \
-    /share/media/sabnzbd/ \
-    /share/media/sonarr/ \
-    /share/media/radarr/ \
-    /share/media/readarr/ \
-    /share/apps/vaultwarden-postgres
+    /share/data
--- scripts/setup_cluster.sh
+++ scripts/setup_cluster.sh
@@ -10,7 +10,7 @@ fi
 # Source the helper script
 source ./scripts/helper_funcs.sh
 
-# --------------------------------START-DEPLOYMENT----------------------------------------
+log_info "-------------------------START-HELM-REPO-ADD/UPDATE-DEPDENCY-BUILD-------------------------"
 
 helm repo add kube-vip https://kube-vip.github.io/helm-charts
 helm repo add longhorn https://charts.longhorn.io
@@ -18,18 +18,28 @@ helm repo add traefik https://traefik.github.io/charts
 # certmanager
 helm repo add jetstack https://charts.jetstack.io
 helm repo add crowdsec https://crowdsecurity.github.io/helm-charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add signoz https://charts.signoz.io
+helm repo add keel https://charts.keel.sh 
+helm repo add vaultwarden https://guerzon.github.io/vaultwarden
+helm repo add uptime-kuma https://helm.irsigler.cloud
 
 helm repo update
+helm dependency update
 
 chart_dir="$script_dir/helm"
-build_helm_dependencies $chart_dir
+build_helm_dependencies "$chart_dir"
+
+log_success "-------------------------DONE-HELM-REPO-ADD/UPDATE-DEPDENCY-BUILD-------------------------"
+
+log_info "-------------------------START-HELM-UPGRADE-------------------------"
 
 helm upgrade bootstrap "$script_dir/helm/1-bootstrap" --install --namespace kube-system -f $script_dir/helm/1-bootstrap/values.yaml -f $script_dir/secrets/values.yaml
 
 helm upgrade edge "$script_dir/helm/2-edge" --install  --namespace edge --create-namespace -f $script_dir/helm/2-edge/values.yaml -f $script_dir/secrets/values.yaml
 helm upgrade edge-custom "$script_dir/helm/3-edge-custom" --install --namespace edge -f $script_dir/secrets/values.yaml
 
-# helm upgrade storage "$script_dir/helm/4-storage" --install --namespace longhorn-system --create-namespace -f $script_dir/helm/4-storage/values.yaml -f $script_dir/secrets/values.yaml
+helm upgrade storage "$script_dir/helm/4-storage" --install --namespace longhorn-system --create-namespace -f $script_dir/helm/4-storage/values.yaml -f $script_dir/secrets/values.yaml
 helm upgrade monitoring "$script_dir/helm/5-monitoring" --install --namespace monitoring --create-namespace -f $script_dir/helm/5-monitoring/values.yaml -f $script_dir/secrets/values.yaml
 
 # helm upgrade media "$script_dir/helm/6-media" --install --create-namespace -f $script_dir/helm/6-media/values.yaml -f $script_dir/secrets/values.yaml --namespace media
@@ -37,3 +47,5 @@ helm upgrade monitoring "$script_dir/helm/5-monitoring" --install --namespace mo
 # kubectl apply -f "$script_dir/helm/apps/nginx.yaml"
 # kubectl apply -f "$script_dir/helm/apps/whoami-local.yaml"
 # kubectl apply -f "$script_dir/helm/apps/whoami-external.yaml"
+
+log_success "-------------------------DONE-HELM-UPGRADE-------------------------"
