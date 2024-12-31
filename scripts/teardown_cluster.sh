27d535d843439582cdbd15897a4ca3ee08b22308
Florian Geckeler <f.geckeler@gmail.com>
2024-12-31 14:08:08 +0100

    more fixes

--- scripts/teardown_cluster.sh
+++ scripts/teardown_cluster.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-helm uninstall media
-helm uninstall monitoring
+# helm uninstall media
+# helm uninstall monitoring
 kubectl -n longhorn-system patch settings.longhorn.io deleting-confirmation-flag --type=merge -p '{"value": "true"}'
-helm uninstall edge-persistence-setup
+kubectl config set-context --current --namespace=default
+helm uninstall db
+kubectl config set-context --current --namespace=kube-system
 helm uninstall bootstrap
