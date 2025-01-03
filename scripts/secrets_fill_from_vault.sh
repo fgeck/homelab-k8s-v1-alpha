7e388456e0090b6a1af95ce8910edc8426522f47
Florian Geckeler <f.geckeler@sap.com>
2025-01-03 15:43:30 +0100

    Fix vaultwarden scripts

--- scripts/secrets_fill_from_vault.sh
+++ scripts/secrets_fill_from_vault.sh
@@ -3,8 +3,9 @@
 set -e
 
 # --------------------------------CONFIG----------------------------------------
-VAULTWARDEN_SERVER=
-SECRET_NAME=KubernetesOnTalos # expect secrets.yaml and values.yaml here
+VAULTWARDEN_SERVER=$(yq '.scriptConfigs.vaultwardenURL' secrets/values.yaml)
+FOLDER_NAME="Homelab"
+SECRET_NAME="KubernetesOnTalos"
 # --------------------------------CONFIG----------------------------------------
 
 # Check if the script is executed at the root of the repository
@@ -13,22 +14,50 @@ if [[ ! -d ".git" ]]; then
   echo -e "${COLOR_RED}[ERROR] This script must be run from the root of the repository.${COLOR_RESET}" >&2
   exit 1
 fi
+
 # Source the helper script
 source ./scripts/helper_funcs.sh
 
-log_debug "Setting your server in config and logging in"
-bw config server "$VAULTWARDEN_SERVER"
+log_info "Setting your server in config and logging in"
+log_exec bw config server "$VAULTWARDEN_SERVER"
 SESSION=$(bw login --raw)
 trap bw_logout EXIT
 
-log_info "Getting secret Kubernetes"
-JSON_RESPONSE=$(bw get item "$SECRET_NAME" --session "$SESSION" --response)
+# Check if the folder exists
+FOLDER_ID=$(bw list folders --session "$SESSION" | jq -r --arg name "$FOLDER_NAME" '.[] | select(.name==$name) | .id')
+
+if [[ -z "$FOLDER_ID" ]]; then
+  log_error "Folder '$FOLDER_NAME' not found."
+  exit 1
+else
+  log_info "Found folder '$FOLDER_NAME' with ID '$FOLDER_ID'."
+fi
+
+# Check if the secret exists in the folder
+ITEM_ID=$(bw list items --session "$SESSION" | jq -r --arg folderId "$FOLDER_ID" --arg name "$SECRET_NAME" '.[] | select(.folderId==$folderId and .name==$name) | .id')
+
+if [[ -z "$ITEM_ID" ]]; then
+  log_error "Item '$SECRET_NAME' not found in folder '$FOLDER_NAME'."
+  exit 1
+fi
+
+log_info "Getting secret '$SECRET_NAME' from folder '$FOLDER_NAME'"
+JSON_RESPONSE=$(bw get item "$ITEM_ID" --session "$SESSION" --response)
 
 log_info "Parsing fields"
 VALUES_BASE64=$(echo "$JSON_RESPONSE" | jq -r '.data.fields[] | select(.name == "values.yaml") | .value')
 SECRETS_BASE64=$(echo "$JSON_RESPONSE" | jq -r '.data.fields[] | select(.name == "secrets.yaml") | .value')
+TALOSCONFIG_BASE64=$(echo "$JSON_RESPONSE" | jq -r '.data.fields[] | select(.name == "talosconfig") | .value')
+CONTROLPLANECONFIG_BASE64=$(echo "$JSON_RESPONSE" | jq -r '.data.fields[] | select(.name == "controlplane.yaml") | .value')
+WORKER1CONFIG_BASE64=$(echo "$JSON_RESPONSE" | jq -r '.data.fields[] | select(.name == "worker-1.yaml") | .value')
+WORKER2CONFIG_BASE64=$(echo "$JSON_RESPONSE" | jq -r '.data.fields[] | select(.name == "worker-2.yaml") | .value')
 
-echo "$VALUES_BASE64" | base64 -d > secrets/values.yaml
-echo "$SECRETS_BASE64" | base64 -d > secrets/secrets.yaml
+log_info "Writing secrets to files"
+echo "$VALUES_BASE64" | base64 -d > test/values.yaml
+echo "$SECRETS_BASE64" | base64 -d > test/secrets.yaml
+echo "$TALOSCONFIG_BASE64" | base64 -d > test/talos/talosconfig
+echo "$CONTROLPLANECONFIG_BASE64" | base64 -d > test/talos/controlplane.yaml
+echo "$WORKER1CONFIG_BASE64" | base64 -d > test/talos/worker-1.yaml
+echo "$WORKER2CONFIG_BASE64" | base64 -d > test/talos/worker-2.yaml
 
-log_success "Successfully wrote secrets/values.yaml and secrets/secrets.yaml"
+log_success "Successfully wrote all secrets from '$SECRET_NAME' in folder '$FOLDER_NAME' to local files"
--- scripts/secrets_push_to_vault.sh
+++ scripts/secrets_push_to_vault.sh
@@ -2,7 +2,8 @@
 
 # --------------------------------CONFIG----------------------------------------
 VAULTWARDEN_SERVER=$(yq '.scriptConfigs.vaultwardenURL' secrets/values.yaml)
-SECRET_NAME=KubernetesOnTalos # Expect secrets.yaml and values.yaml here
+FOLDER_NAME="Homelab"
+SECRET_NAME="KubernetesOnTalos"
 # --------------------------------CONFIG----------------------------------------
 
 # Check if the script is executed at the root of the repository
@@ -26,18 +27,29 @@ CONTROLPLANECONFIG_BASE64=$(cat secrets/talos/controlplane.yaml | base64)
 WORKER1CONFIG_BASE64=$(cat secrets/talos/worker-1.yaml | base64)
 WORKER2CONFIG_BASE64=$(cat secrets/talos/worker-2.yaml | base64)
 
-GET_RESPONSE=$(bw get item "$SECRET_NAME" --session "$SESSION" --response)
-SUCCESS=$(echo $GET_RESPONSE | jq -r '.success')
+# Check if the folder exists or create it
+FOLDER_ID=$(bw list folders --session "$SESSION" | jq -r --arg name "$FOLDER_NAME" '.[] | select(.name==$name) | .id')
 
-if [[ "$SUCCESS" == "false" ]]; then
-    log_debug "Item with name '$SECRET_NAME' not found."
-    log_info "Creating new item with name '$SECRET_NAME'"
+if [[ -z "$FOLDER_ID" ]]; then
+    FOLDER_ID=$(bw get template folder | jq ".name=\"$FOLDER_NAME\"" | bw encode | bw create folder --session "$SESSION" | jq -r '.id')
+    log_info "Created folder '$FOLDER_NAME' with ID '$FOLDER_ID'."
+else
+    log_info "Found folder '$FOLDER_NAME' with ID '$FOLDER_ID'."
+fi
+
+# Check if the item exists within the folder
+ITEM_ID=$(bw list items --session "$SESSION" | jq -r --arg folderId "$FOLDER_ID" --arg name "$SECRET_NAME" '.[] | select(.folderId==$folderId and .name==$name) | .id')
+
+if [[ -z "$ITEM_ID" ]]; then
+    log_info "Item with name '$SECRET_NAME' not found in folder '$FOLDER_NAME'."
+    log_info "Creating new item with name '$SECRET_NAME' in folder '$FOLDER_NAME'"
 
     bw get template item --session "$SESSION" | \
     jq ".name=\"$SECRET_NAME\" |
         .type = 2 |
+        .folderId=\"$FOLDER_ID\" |
         .secureNote.type = 0 |
-        .notes=\"Base64 encoded Kubernetes values and TalOS secrets\" |
+        .notes=\"Base64 encoded Kubernetes on TalOS secrets\" |
         .fields += [
             {\"name\": \"values.yaml\", \"value\": \"$VALUES_BASE64\", \"type\": 1},
             {\"name\": \"secrets.yaml\", \"value\": \"$SECRETS_BASE64\", \"type\": 1},
@@ -48,11 +60,9 @@ if [[ "$SUCCESS" == "false" ]]; then
         ]" | \
     bw encode | \
     bw create item --session "$SESSION" > /dev/null
-
 else
-    ITEM_ID=$(echo "$GET_RESPONSE" | jq -r '.data.id')
-    log_debug "Item with name '$SECRET_NAME' and ID '$ITEM_ID' found."
-    log_info "Updating existing item with name '$SECRET_NAME'"
+    log_info "Item with name '$SECRET_NAME' and ID '$ITEM_ID' found in folder '$FOLDER_NAME'."
+    log_info "Updating existing item with name '$SECRET_NAME' in folder '$FOLDER_NAME'"
 
     bw get item "$ITEM_ID" --session "$SESSION" | \
     jq ".fields = [
@@ -67,4 +77,4 @@ else
     bw edit item "$ITEM_ID" --session "$SESSION" > /dev/null
 fi
 
-log_success "Successfully pushed all TalOS and Kubernetes secrets to $VAULTWARDEN_SERVER"
+log_success "Successfully pushed all TalOS and Kubernetes secrets to $VAULTWARDEN_SERVER in folder '$FOLDER_NAME'"
