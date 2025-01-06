ba1ec9c49426fd359db3f19aac39ad19a08cea45
Florian Geckeler <f.geckeler@gmail.com>
2025-01-06 22:43:40 +0100

    add monitoring to teardown

--- scripts/cluster/teardown.sh
+++ scripts/cluster/teardown.sh
@@ -8,6 +8,7 @@ gum confirm "Are you sure you want to teardown the whole cluster?" || exit 1
 kubectl config set-context --current --namespace=media
 helm uninstall media
 kubectl config set-context --current --namespace=monitoring
+helm uninstall monitoring
 kubectl config set-context --current --namespace=security
 helm uninstall security
 kubectl config set-context --current --namespace=default
