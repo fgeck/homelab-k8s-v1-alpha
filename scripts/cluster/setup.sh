34680137bec0b89b865c1643c87b37cca00e214f
Florian Geckeler <43751896+fgeck@users.noreply.github.com>
2025-01-06 20:39:04 +0100

    Introduce Postgres for Security (#23)

--- scripts/cluster/setup.sh
+++ scripts/cluster/setup.sh
@@ -65,22 +65,24 @@ log_info "START --> Deploying Databases                                         
 log_info "------------------------------------------------------------------------------------------"
 echo ""
 log_exec kubectl config set-context --current --namespace=default
-log_exec helm upgrade persistence "$script_dir/helm/2-persistence" --install  -f $script_dir/helm/2-persistence/values.yaml -f $script_dir/secrets/values.yaml
+log_exec helm upgrade default-persistence "$script_dir/helm/2-persistence" --install  -f $script_dir/helm/2-persistence/default-values.yaml -f $script_dir/secrets/values.yaml
+log_exec kubectl config set-context --current --namespace=security
+log_exec helm upgrade security-persistence "$script_dir/helm/2-persistence" --install  -f $script_dir/helm/2-persistence/security-values.yaml -f $script_dir/secrets/values.yaml
 log_success "------------------------------------------------------------------------------------------"
 log_success "DONE --> Deploying Databases                                                             |"
 log_success "------------------------------------------------------------------------------------------"
 echo ""
 
-log_info "------------------------------------------------------------------------------------------"
-log_info "START --> Deploying Security Stack                                                            |"
-log_info "------------------------------------------------------------------------------------------"
-echo ""
-log_exec kubectl config set-context --current --namespace=security
-log_exec helm upgrade persistence "$script_dir/helm/3-security" --install  -f $script_dir/helm/3-security/values.yaml -f $script_dir/secrets/values.yaml
-log_success "------------------------------------------------------------------------------------------"
-log_success "DONE --> Deploying Security Stack                                                             |"
-log_success "------------------------------------------------------------------------------------------"
-echo ""
+# log_info "------------------------------------------------------------------------------------------"
+# log_info "START --> Deploying Security Stack                                                            |"
+# log_info "------------------------------------------------------------------------------------------"
+# echo ""
+# log_exec kubectl config set-context --current --namespace=security
+# log_exec helm upgrade persistence "$script_dir/helm/3-security" --install  -f $script_dir/helm/3-security/values.yaml -f $script_dir/secrets/values.yaml
+# log_success "------------------------------------------------------------------------------------------"
+# log_success "DONE --> Deploying Security Stack                                                             |"
+# log_success "------------------------------------------------------------------------------------------"
+# echo ""
 
 log_info "------------------------------------------------------------------------------------------"
 log_info "START --> Deploying Monitoring: Uptime-Kuma, Signoz                                      |"
